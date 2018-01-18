294e4b7dd376d67afd4a3d50317ce787846a778f
Sophie Kane <skane0207@gmail.com>
2018-01-18 01:41:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = "I don't do much."
 kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
-  kittens.unshift
+  kittens.unshift(name)
+  return kittens
   
 }
